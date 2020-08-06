e003a0c138e040b1368d24e1f3e31dafdbfd061a
Jessica <jblau1993@gmail.com>
2020-08-06 16:54:50 +0300

    "question2"

--- q_1/q1_313628364.c
+++ q_1/q1_313628364.c
@@ -1,38 +1,35 @@
 #define _BSD_SOURCE
-#include <stdlio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-char globBuf[65536];            /* 1. Where is allocated? */
-int primes[] = { 2, 3, 5, 7 };  /* 2. Where is allocated? */
+char globBuf[65536];            /* 1. BSS */
+int primes[] = { 2, 3, 5, 7 };  /* 2. data  */
 
-static int
-square(int x)                   /* 3. Where is allocated? */
+static int square(int x)                   /* 3. text */
 {
-    int result;                 /* 4. Where is allocated? */
+    int result;                 /* 4. stack */
 
     result = x * x;
-    return result;              /* 5. How the return value is passed? */
+    return result;              /* 5. result is passed from stack to the caller and saved in the stack*/
 }
 
-static void
-doCalc(int val)                 /* 6. Where is allocated? */
+static void doCalc(int val)                 /* 6. text */
 {
     printf("The square of %d is %d\n", val, square(val));
 
     if (val < 1000) {
-        int t;                  /* 7. Where is allocated? */
+        int t;                 /* 7. stack */
 
         t = val * val * val;
-        printf("The cube of %d is %d\n", val, t);
+        printf("The cube of %d is %d\n", val);
     }
 }
 
-int
-main(int argc, char* argv[])    /* Where is allocated? */
+int main(int argc, char* argv[])    /* 8. text */
 {
-    static int key = 9973;      /* Where is allocated? */
-    static char mbuf[10240000]; /* Where is allocated? */
-    char* p;                    /* Where is allocated? */
+    static int key = 9973;      /* 9. data */
+    static char mbuf[10240000]; /* 10. BSS */
+    char* p;                    /* 11. stack */
 
 
     doCalc(key);
---
+++ q_2/q_21/check_pid.c
@@ -0,0 +1,21 @@
+#include <errno.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <signal.h>
+
+
+int main(int argc, char* argv[])
+{
+    if (kill(atoi(argv[1]), 0)== -1){
+        if(errno == EPERM){
+            printf ("Process %s exists but we have no permission\n", argv[1]);
+            return 0;
+        }
+        printf ("The process %s doesn't exist\n", argv[1]);
+    }
+    else{
+        printf ("process %s exists\n", argv[1]);
+    }
+    
+    return 0;
+}
